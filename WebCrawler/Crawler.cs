59b3ed7e841352fcdbbc3698c955194ace046044
rocmeister <lw31@rice.edu>
2019-08-06 18:33:27 -0500

    The crawler works fine now after adding try catch error handling, code needs to be cleaned up

---
+++ WebCrawler/Crawler.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace WebCrawler
+{
+    public class Crawler
+    {
+        private Hashtable _urls;
+        private int _numPages = 0;
+
+        public Crawler(string url)
+        {
+            // Add the new url to the initial hashtable
+            _urls = new Hashtable();
+            _urls[url] = false; // or _urls.Add(url, false);
+        }
+
+        public void Crawl()
+        {
+            Console.WriteLine("Start crawling...");
+            while (true)
+            {
+                string candidateURL = null;
+                // Go through the url hashtable
+                // Find an unprocessed url from HT and download its contents
+                foreach (string url in _urls.Keys)
+                {
+                    if (!(bool)_urls[url])
+                    {
+                        candidateURL = url;
+                        break;
+                    }
+
+                    //if ((bool)_urls[url]) continue; //??????????????????????
+                    //candidateURL = url;
+                }
+                if (candidateURL == null || _numPages > 10) break;
+
+                Console.WriteLine("Analyzing {0}...", candidateURL);
+
+                // Download the content in string format
+                string html = Download(candidateURL);
+
+                _urls[candidateURL] = true;
+                _numPages++;
+
+                // Parse the downloaded html string and store further links
+                // used in that html page
+                Parse(html);
+            }
+            Console.WriteLine("Finished crawling...");
+        }
+
+        // Downloads the html page content from the given url
+        private string Download(string url)
+        {
+            try
+            {
+                //Console.WriteLine("Download function using {0}", url);
+                // Use WebClient to download the page data
+                WebClient client = new WebClient();
+                byte[] data = client.DownloadData(url);
+                // Writes the html content to a new file
+                FileStream fs = new FileStream(_numPages.ToString(), FileMode.OpenOrCreate);
+                fs.Write(data, 0, data.Length);
+                fs.Close();
+                //return client.DownloadString(url);
+                return Encoding.UTF8.GetString(data);
+            }
+            catch
+            {
+            }
+            return "";
+            //Console.WriteLine("Download function using {0}", url);
+            //// Use WebClient to download the page data
+            //WebClient client = new WebClient();
+            //byte[] data = client.DownloadData(url);
+            //// Writes the html content to a new file
+            //FileStream fs = new FileStream(_numPages.ToString(), FileMode.OpenOrCreate);
+            //fs.Write(data, 0, data.Length);
+            //fs.Close();
+            ////return client.DownloadString(url);
+            //return Encoding.UTF8.GetString(data);
+
+            // Other ways to download the html contents from a web page
+
+            // Download the page data array then convert to a string
+            //byte[] data = client.DownloadData(url);
+            //return Encoding.UTF8.GetString(data);
+
+            // Use WebRequest/WebResponse
+            //HttpWebRequest req = (HttpWebRequest) WebRequest.Create(url);
+            //HttpWebResponse resp = (HttpWebResponse) req.GetResponse();
+            //Stream stream = resp.GetResponseStream();
+            //return new StreamReader(stream).ReadToEnd();
+        }
+
+        // Parse the given html of a page and find the url links 
+        // referenced and sourced in this page
+        private void Parse(string html)
+        {
+            string strRef = @"(href|HREF|src|SRC)[ ]*=[ ]*[""'][^""'#>]+[""']";
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim('"', '\'', '#', ' ', '>');
+                if (strRef.Length == 0) continue;
+
+                if (_urls[strRef] == null) _urls[strRef] = false;
+            }
+        }
+
+        
+    }
+}
---
+++ WebCrawler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace WebCrawler
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // starts with a user-inputted url
+            // or Rice University's url by default
+            string defaultURL = "https://www.rice.edu/";
+            string url = args.Length >= 1 ? args[0] : defaultURL;
+            Crawler crawler = new Crawler(url);
+            //Crawler crawler = new Crawler();
+            //crawler.urls.Add(defaultURL, false);
+
+            // start running the crawler executing on a new thread
+            new Thread(new ThreadStart(crawler.Crawl)).Start();
+
+            Console.ReadLine();
+        }
+    }
+}
